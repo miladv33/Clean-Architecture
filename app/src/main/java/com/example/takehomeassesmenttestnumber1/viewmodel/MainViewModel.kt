e4f3ce0ced078db316f6e80e3707db8663c913fa
Milad Mosavi <milad.mosavi.developer@gmail.com>
2022-05-26 23:08:58 +0430

    add test base class and it's library

--- app/src/main/java/com/example/takehomeassesmenttestnumber1/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/takehomeassesmenttestnumber1/viewmodel/MainViewModel.kt
@@ -1,9 +1,6 @@
 package com.example.takehomeassesmenttestnumber1.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.takehomeassesmenttestnumber1.model.RandomQuote
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
