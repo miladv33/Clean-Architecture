cc5c971b9b1f6552ca3f08718b4fca418accc191
Milad Mosavi <milad.mosavi.developer@gmail.com>
2022-05-26 23:01:48 +0430

    add model

---
+++ app/src/main/java/com/example/takehomeassesmenttestnumber1/model/RandomQuote.kt
@@ -0,0 +1,24 @@
+package com.example.takehomeassesmenttestnumber1.model
+
+/**
+ *
+ * @property _id String
+ * @property author String
+ * @property authorSlug String
+ * @property content String
+ * @property dateAdded String
+ * @property dateModified String
+ * @property length Int
+ * @property tags List<String>
+ * @constructor
+ */
+data class RandomQuote(
+    val _id: String,
+    val author: String,
+    val authorSlug: String,
+    val content: String,
+    val dateAdded: String,
+    val dateModified: String,
+    val length: Int = -1,
+    val tags: List<String>,
+)
