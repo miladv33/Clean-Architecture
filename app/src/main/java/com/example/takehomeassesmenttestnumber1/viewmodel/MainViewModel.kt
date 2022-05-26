7975d0a44f868b55db3c56b427c77e9f8d813df7
Milad Mosavi <milad.mosavi.developer@gmail.com>
2022-05-26 23:05:08 +0430

    add view model

---
+++ app/src/main/java/com/example/takehomeassesmenttestnumber1/viewmodel/MainViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.takehomeassesmenttestnumber1.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.takehomeassesmenttestnumber1.model.RandomQuote
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(): ViewModel() {
+
+}
