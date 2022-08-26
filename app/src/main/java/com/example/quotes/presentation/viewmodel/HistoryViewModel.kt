ec783accc721f47c2cf727434ade81b53d3e9a4b
Mousavi <miladv33@gmail.com>
2022-08-26 18:29:48 +0430

    add history and second pages

---
+++ app/src/main/java/com/example/quotes/presentation/viewmodel/HistoryViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.quotes.presentation.viewmodel
+
+import androidx.compose.runtime.remember
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import com.example.quotes.presentation.base.BaseViewModel
+
+class HistoryViewModel: BaseViewModel() {
+
+
+
+
+}
--- app/src/main/java/com/example/quotes/ui/secondpage/SecondPage.kt
+++ app/src/main/java/com/example/quotes/ui/secondpage/SecondPage.kt
@@ -2,9 +2,16 @@ package com.example.quotes.ui.secondpage
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
 
 @Composable
 fun SecondPage() {
+    History()
+}
 
+@Composable
+fun History() {
     Text(text = "second page")
 }
